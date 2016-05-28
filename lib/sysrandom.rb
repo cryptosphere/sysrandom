b710cb1bf7d71ee654560c44f08a1daff38178aa
Tony Arcieri <bascule@gmail.com>
2016-05-28 16:41:58 -0700

    Implement the full SecureRandom API
    Adds the following methods:
    
    * #base64
    * #hex
    * #urlsafe_base64
    * #uuid

--- lib/sysrandom.rb
+++ lib/sysrandom.rb
@@ -1,8 +1,13 @@
 require "sysrandom/version"
 
+require "base64"
+
 # Secure random number generation using system RNG facilities
 module Sysrandom
   module_function
+
+  # For some reason SecureRandom defaults to 16 bytes
+  DEFAULT_LENGTH = 16
 
   if defined?(JRUBY_VERSION)
     require "java"
@@ -18,7 +23,7 @@ module Sysrandom
       @_java_secure_random.nextLong & 0xFFFFFFFF
     end
 
-    def random_bytes(n = 16)
+    def random_bytes(n = DEFAULT_LENGTH)
       raise ArgumentError, "negative string size" if n < 0
 
       bytes = Java::byte[n].new
@@ -38,5 +43,22 @@ module Sysrandom
       result *= n
       n.is_a?(Fixnum) ? result.floor : result
     end
+  end
+
+  def base64(n = DEFAULT_LENGTH)
+    Base64.encode64(random_bytes(n)).chomp
+  end
+
+  def urlsafe_base64(n = DEFAULT_LENGTH)
+    Base64.urlsafe_encode64(random_bytes(n)).chomp
+  end
+
+  def hex(n = DEFAULT_LENGTH)
+    random_bytes(n).unpack("h*").first
+  end
+
+  def uuid
+    values = SecureRandom.hex(16).match(/\A(.{8})(.{4})(.)(.{3})(.)(.{3})(.{12})\z/)
+    "#{values[1]}-#{values[2]}-4#{values[4]}-#{'89ab'[values[5].ord % 4]}#{values[6]}-#{values[7]}"
   end
 end
